959b493d9652df168c2c364d55fdaa9307b3d719
Julien Allegre <julien.allegre@videomenthe.fr>
2020-12-09 18:08:34 +0100

    My first action is ready

---
+++ index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(async function () {
+  try {
+    const githubToken = core.getInput('github_token', { required: true });
+    const labels = core.getInput('labels').split('\n').filter(label => (label && label.length));
+
+    const [owner, repo] = github.context.owner
+
+    if (labels.length === 0) {
+      return;
+    }
+
+    const gitHubClient = new github.GitHub(githubToken);
+    console.error('*****ju***** index.js.32','github.context.owner:', github.context.owner);
+    console.error('*****ju***** index.js.32','github.context.repo:', github.context.owner);
+    console.error('*****ju***** index.js.34','github.context.issue.number:', github.context.issue.number);
+    console.error('*****ju***** index.js.35','github.context.issue.labels:', github.context.issue.labels);
+    for (const label of labels) {
+
+      try{
+        await gitHubClient.issues.removeLabel({
+          name: label,
+          owner: github.context.owner,
+          repo: github.context.repo,
+          issue_number: github.context.issue.number
+        });
+  
+      }catch(err){
+        //label not found continue
+      }
+    }
+  } catch (e) {
+    core.error(e);
+    core.setFailed(e.message);
+  }
+})()
+
