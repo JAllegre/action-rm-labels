48fb55dc3d40f1f147305c1076dbb4d9a9eee85f
Julien Allegre <julien.allegre@videomenthe.fr>
2020-12-09 20:47:55 +0100

    up

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 (async function () {
   try {
-    const githubToken = core.getInput('github_token', { required: true });
-    const labels = (core.getInput('labels') || '').split(',').filter(label => (label && label.length));
+    const githubToken = core.getInput("github_token", { required: true });
+    const labels = (core.getInput("labels") || "")
+      .split(",")
+      .filter((label) => label && label.length);
 
-//    const [owner, repo] = github.context.owner
-    console.error('*****ju***** index.js.8','labels', labels);
-    core.error('*****ju***** index.js.7','labels', labels);
-    
     if (labels.length === 0) {
       return;
     }
-    
-    const gitHubClient = github.getOctokit(githubToken);
-    console.error('*****ju***** index.js.32','github.context.owner:', github.context.owner);
-    console.error('*****ju***** index.js.32','github.context.repo:', github.context.owner);
-    console.error('*****ju***** index.js.34','github.context.issue.number:', github.context.issue.number);
-    console.error('*****ju***** index.js.35','github.context.issue.labels:', github.context.issue.labels);
-    for (const label of labels) {
 
-      try{
+    const gitHubClient = github.getOctokit(githubToken);
+    console.error(
+      "*****ju***** index.js.32",
+      "github.context.repo:",
+      github.context.owner
+    );
+    console.error(
+      "*****ju***** index.js.34",
+      "github.context.issue.number:",
+      github.context.issue.number
+    );
+    console.error(
+      "*****ju***** index.js.35",
+      "github.context.issue.labels:",
+      github.context.issue.labels
+    );
+    for (const label of labels) {
+      try {
         await gitHubClient.issues.removeLabel({
           name: label,
           owner: github.context.owner,
           repo: github.context.repo,
-          issue_number: github.context.issue.number
+          issue_number: github.context.issue.number,
         });
-  
-      }catch(err){
+      } catch (err) {
+        core.error(err);
         //label not found continue
       }
     }
@@ -37,5 +45,4 @@ const github = require('@actions/github');
     core.error(e);
     core.setFailed(e.message);
   }
-})()
-
+})();
